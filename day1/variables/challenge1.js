b96151f372f599014b428fac7af170e331504650
meryamajgagal <maryamajgagale826@gmail.com>
2026-09-01 11:06:37 +0200

    challenge1

---
+++ day1/variables/challenge1.js
@@ -0,0 +1,10 @@
+let Pseudo = "ShadowX";
+let Niveau = 12;
+let age = 21;
+let Score = 4580;
+let pays = "Maroc";
+console.log(Pseudo)
+console.log(Niveau)
+console.log(age)
+console.log(Score)
+console.log(pays)
