0b43f1c47fada63c73fb717bb7e62419f682ee7d
meryamajgagal <maryamajgagale826@gmail.com>
2026-09-01 14:21:21 +0200

    challenge2

---
+++ day1/variables/Challenge2.js
@@ -0,0 +1,5 @@
+const  prompt = require("prompt-sync")();
+let dirhams = prompt("enter the amount in dirhams: ");
+eur = dirhams / 11;
+console.log("Badget en mad :",dirhams);
+console.log("The badget in eur: " ,eur);
