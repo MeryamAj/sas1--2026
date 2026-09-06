cce69ad0d3bfc4914b4d66a482ad0691e6fbced3
MeryamAj <maryamajgagale826@gmail.com>
2026-09-06 18:18:51 +0100

    challenge1

---
+++ day5/challenge1.js
@@ -0,0 +1,16 @@
+let prompt = require("prompt-sync")();
+
+
+function afficherElements(tab)
+{
+    let i = 0;
+    while(i < tab.length)
+    {
+        console.log(tab[i]);
+        i++;
+    }
+}
+
+afficherElements([10, 20, 30]);
+
+
