36a700d99f827a662d77aa927747bfa42ab86d73
MeryamAj <maryamajgagale826@gmail.com>
2026-09-06 23:49:09 +0100

    update

--- day4/challenge9.js
+++ day4/challenge9.js
@@ -2,8 +2,23 @@ let prompt = require("prompt-sync")();
 
 function nettoyerEspaces(chaine)
 {
-    let result = chaine.trim();
-    return result;
+    let newStr = "";
+    let first = 0;
+    let end = chaine.length - 1;
+    while(chaine[first] === ' ')
+    {
+        first++;
+    }
+    while(chaine[end] === ' ')
+    {
+        end--;
+    }
+    while(first <= end)
+    {
+        newStr += chaine[first];
+        first++;
+    }
+    return newStr
 }
 
 let chaine = prompt("enter chaine : ");
